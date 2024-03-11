1dc9bbbe9ef7a5e4af0dce0f2b933ec90202ca40
jh-zzzzz <jacobheinegard@hotmail.com>
2024-03-11 11:05:15 +0100

    add static jsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Techniques Tally</h1>
+      <select>
+        <option>Football</option>
+        <option>BJJ</option>
+      </select>
+      <button>Add new technique</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Technique</th>
+            <th>total # of occurences</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Nutmeg</td>
+            <td>1</td>
+          </tr>
+          <tr>
+            <td>Step over</td>
+            <td>1</td>
+          </tr>
+        </tbody>
+      </table>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
