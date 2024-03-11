233e8794b0cdac55a60c1021405fc27f475779f0
jh-zzzzz <jacobheinegard@hotmail.com>
2024-03-11 11:22:07 +0100

    break out nav and techniques table in to different components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { Nav } from "./components/Nav";
+import { TechniquesTable } from "./components/TechniquesTable";
 
 function App() {
   return (
     <>
-      <h1>Techniques Tally</h1>
+      <Nav />
       <select>
         <option>Football</option>
         <option>BJJ</option>
       </select>
       <button>Add new technique</button>
-      <table>
-        <thead>
-          <tr>
-            <th>Technique</th>
-            <th>total # of occurences</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Nutmeg</td>
-            <td>1</td>
-          </tr>
-          <tr>
-            <td>Step over</td>
-            <td>1</td>
-          </tr>
-        </tbody>
-      </table>
+      <TechniquesTable />
     </>
   );
 }
---
+++ frontend/src/components/TechniquesTable.tsx
@@ -0,0 +1,24 @@
+export const TechniquesTable = () => {
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>Technique</th>
+            <th>total # of occurences</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Nutmeg</td>
+            <td>1</td>
+          </tr>
+          <tr>
+            <td>Step over</td>
+            <td>1</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
+};
