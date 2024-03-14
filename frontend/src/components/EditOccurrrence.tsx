9718b393e875314ed85cbafcde256e8937f111ba
jh-zzzzz <jacobheinegard@hotmail.com>
2024-03-14 23:01:28 +0100

    stylin

--- frontend/src/components/EditOccurrrence.tsx
+++ frontend/src/components/EditOccurrrence.tsx
@@ -1,5 +1,5 @@
 import { FormEvent, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { getOccurrence, updateOccurrence } from "../http";
 
 type EditOccurrenceEvent = FormEvent<HTMLFormElement> & {
@@ -10,14 +10,13 @@ type EditOccurrenceEvent = FormEvent<HTMLFormElement> & {
 };
 
 export const EditOccurrence = () => {
-  const { id } = useParams();
+  const { sport, technique, id } = useParams();
   const [occurrence, setOccurrence] = useState();
 
   useEffect(() => {
     getOccurrence(id!)
       .then((resp) => resp.json())
       .then((data) => setOccurrence(data))
-      .then(() => console.log(occurrence))
       .catch(() => console.log("couldnt fetch occurrence"));
   }, []);
 
@@ -36,70 +35,78 @@ export const EditOccurrence = () => {
   };
 
   return (
-    occurrence && (
-      <>
-        <p>Editing occurrence {occurrence.id}</p>
-        <form onSubmit={handleOnSubmit}>
-          <label htmlFor="date">Date</label>
-          <input
-            type="date"
-            id="date"
-            defaultValue={occurrence.date}
-            disabled
-          />
+    <>
+      <p>
+        <Link to="/">Home</Link> {`> ${sport} > `}{" "}
+        <Link to={`/${sport}/${technique}`}>{`${technique}`}</Link>
+        {` > Edit occurrence`}
+      </p>
+      {occurrence && (
+        <>
+          <p>Editing occurrence {occurrence.id}</p>
+          <form onSubmit={handleOnSubmit}>
+            <label htmlFor="date">Date</label>
+            <input
+              type="date"
+              id="date"
+              defaultValue={occurrence.date}
+              disabled
+            />
 
-          <label htmlFor="athlete">Athlete</label>
-          <input
-            type="text"
-            id="athlete"
-            defaultValue={occurrence.athlete}
-            disabled
-          />
+            <label htmlFor="athlete">Athlete</label>
+            <input
+              type="text"
+              id="athlete"
+              defaultValue={occurrence.athlete}
+              disabled
+            />
 
-          <label htmlFor="game">Game</label>
-          <input
-            type="text"
-            id="game"
-            defaultValue={occurrence.game}
-            disabled
-          />
+            <label htmlFor="game">Game</label>
+            <input
+              type="text"
+              id="game"
+              defaultValue={occurrence.game}
+              disabled
+            />
 
-          <label htmlFor="timestamp">Timestamp</label>
-          <input
-            type="text"
-            id="timestamp"
-            defaultValue={occurrence.timestamp}
-            required
-          />
+            <label htmlFor="timestamp">Timestamp</label>
+            <input
+              type="text"
+              id="timestamp"
+              defaultValue={occurrence.timestamp}
+              required
+            />
 
-          <label htmlFor="videoLink">Video link</label>
-          <input
-            type="url"
-            id="videoLink"
-            defaultValue={occurrence.videoLink}
-          />
+            <label htmlFor="videoLink">Video link</label>
+            <input
+              type="url"
+              id="videoLink"
+              defaultValue={occurrence.videoLink}
+            />
 
-          <input type="submit" />
-        </form>
-        <table>
-          <thead>
-            <tr>
-              <th>Edit history</th>
-            </tr>
-          </thead>
-          <tbody>
-            {occurrence.edits.map((edit) => (
-              <tr key={edit.editedAt}>
-                <td>{edit.editedAt}</td>
+            <input type="submit" value="Edit" className="edit" />
+            <input type="button" value="Cancel" />
+          </form>
+          <table>
+            <thead>
+              <tr>
+                <th>Edit history</th>
               </tr>
-            ))}
-            <tr>
-              <td>{occurrence.createdAt}</td>
-              <td>(created)</td>
-            </tr>
-          </tbody>
-        </table>
-      </>
-    )
+            </thead>
+            <tbody>
+              {occurrence.edits.map((edit) => (
+                <tr key={edit.editedAt}>
+                  <td>{edit.editedAt}</td>
+                </tr>
+              ))}
+              <tr>
+                <td>{occurrence.createdAt}</td>
+                <td>(created)</td>
+              </tr>
+            </tbody>
+          </table>
+        </>
+      )}
+    </>
   );
 };
