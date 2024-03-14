d803a15d43b8a62e014011e46cef14a8ecbef094
jh-zzzzz <jacobheinegard@hotmail.com>
2024-03-14 10:26:01 +0100

    add total # of occ to technique

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -9,7 +9,7 @@ VALUES
 INSERT INTO techniques
     (sport_key, total_number_of_occurrences, name)
 VALUES
-    ('Football', 0, 'Step over'),
+    ('Football', 2, 'Step over'),
     ('Football', 0, 'Nutmeg'),
     ('BJJ', 0, 'Bow and arrow choke'),
     ('Basketball', 0, 'Cross over');
